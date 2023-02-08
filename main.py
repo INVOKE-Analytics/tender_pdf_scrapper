0a3ff31df43971ec61eaab292ccb9d3b192dc37c
Ammar-Azman <muhammarsafuan@gmail.com>
2023-02-08 17:04:32 +0800

    add download button

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ st.write('Source: https://msmart.mcmc.gov.my/')
 df = get_df('tenderquoted')
 
 now = datetime.now()
-with open(f'tender_history_{now}.csv') as f:
-    st.download_button('Download CSV', f)
+csv = df.to_csv(index=False).encode('utf-8')
+st.download_button('Download CSV', csv, 'tender_history_{now}.csv', 'text/csv')
 
 st.table(df)
