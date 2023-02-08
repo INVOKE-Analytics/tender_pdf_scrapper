8ccec6d2bc990d7c982d1c3f912842493bfa42df
Ammar-Azman <muhammarsafuan@gmail.com>
2023-02-08 16:36:52 +0800

    update main.py

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from scrapper import get_df
 import streamlit as st 
+from datetime import datetime
 
 st.title("Tender History")
 st.write('Source: https://msmart.mcmc.gov.my/')
 df = get_df('tenderquoted')
+
+now = datetime.now()
+with open(f'tender_history_{now}.csv') as f:
+    st.download_button('Download CSV', f)
+
 st.table(df)
